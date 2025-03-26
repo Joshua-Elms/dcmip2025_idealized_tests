9bce8a3a856af4d78d73b41540dadc2cd8be4c8c
Joshua-Elms <joshua.elms111@gmail.com>
2025-03-26 10:46:07 -0600

    minor script changes to debug graphcast_small

--- inference/python_scripts/test_graphcast_era5.py
+++ inference/python_scripts/test_graphcast_era5.py
@@ -10,6 +10,7 @@ time_loop  = get_model("e2mip://graphcast", device="cuda:0")
 output_path = "era5_ic_graphcast.nc"
 print(f"output_path: {output_path}")
 data_source = cds.DataSource(time_loop.in_channel_names)
-ds = run_basic_inference(time_loop, n=4, data_source=data_source, time=datetime.datetime(2018, 1, 2))
+print(time_loop.in_channel_names)
+ds = run_basic_inference(time_loop, n=2, data_source=data_source, time=datetime.datetime(2018, 2, 2))
 ds.to_netcdf(output_path)
 print(f"output: {ds}")
