a9b80669fefe35d7048175302cf5fbf7a4650b52
Joshua-Elms <joshua.elms111@gmail.com>
2025-04-16 12:45:48 -0600

    almost done with energy conservation

--- experiments/one_step_energy_conservation/3.analysis.py
+++ experiments/one_step_energy_conservation/3.analysis.py
@@ -4,9 +4,8 @@ import numpy as np
 import yaml
 import matplotlib.pyplot as plt
 import datetime as dt
-import calendar
 from utils import inference, vis
-import math
+import scipy
 from time import perf_counter
 from matplotlib import colormaps
 
@@ -39,6 +38,7 @@ cmap_str = "Set2" # options here: matplotlib.org/stable/tutorials/colors/colorma
 cp = 1005.0  # J/kg/K
 g = 9.81  # m/s^2j
 Lv = 2.26e6  # J/kg
+sb_const = 5.670374419e-8  # W/m^2/K^4, from https://physics.nist.gov/cgi-bin/cuu/Value?sigma
 ##############################################
 
 
@@ -50,42 +50,107 @@ print(f"Loading OLR data from {olr_save_path}")
 olr_ds = xr.open_dataset(olr_save_path)
 ##############################################
 
-### Sanity Test Plots ###
+# ### Sanity Test Plots ###
 
-# VAR_2T over range of delta_Ts
-title_str = "2m Temperature for $\Delta T={delta_T}$ K"
-titles = [title_str.format(delta_T=delta_T) for delta_T in ds["delta_T"].values]
+# # VAR_2T over range of delta_Ts
+# title_str = "2m Temperature for $\Delta T={delta_T}$ K"
+# titles = [title_str.format(delta_T=delta_T) for delta_T in ds["delta_T"].values]
 
-vis.create_and_plot_variable_gif(
-    data=ds["VAR_2T"].isel(init_time=0, lead_time=0),
-    plot_var="VAR_2T",
-    iter_var="delta_T",
-    iter_vals=[0, 1, 2],
-    plot_dir=plot_dir,
-    units="degrees K",
-    cmap="magma",
-    titles=titles,
-    keep_images=True,
-    dpi=300,
-    fps=1,
+# vis.create_and_plot_variable_gif(
+#     data=ds["VAR_2T"].isel(init_time=0, lead_time=0),
+#     plot_var="VAR_2T",
+#     iter_var="delta_T",
+#     iter_vals=[0, 1, 2],
+#     plot_dir=plot_dir,
+#     units="degrees K",
+#     cmap="magma",
+#     titles=titles,
+#     keep_images=True,
+#     dpi=300,
+#     fps=1,
+# )
+
+# # VAR_OLR over range of times
+# titles = [f"OLR at {np.datetime_as_string(time, unit='h')} UTC" for time in olr_ds["init_time"].values]
+# vis.create_and_plot_variable_gif(
+#     data=olr_ds["VAR_OLR"],
+#     plot_var="VAR_OLR",
+#     iter_var="init_time",
+#     iter_vals=[0, 1],
+#     plot_dir=plot_dir,
+#     units="W/m^2",
+#     cmap="magma",
+#     titles=titles,
+#     keep_images=True,
+#     dpi=300,
+#     fps=1,
+# )
+# ######################################
+
+### Analysis ###
+
+# Step 1: Calculate the global mean OLR for each init time
+
+olr_ds["MEAN_OLR"] = inference.latitude_weighted_mean(olr_ds["VAR_OLR"], olr_ds["latitude"])
+
+# Step 2: Calculate the effective radiative temperature of the Earth from the OLR
+olr_ds["ERT"] = (olr_ds["MEAN_OLR"] / sb_const) ** (1/4)
+
+# Step 3: Calculate the total energy of the atmosphere
+
+### Step 3a: Compute a mask of the p-levels above the surface for integration
+sp = ds["SP"]
+sp_expanded = sp.expand_dims(dim={"level": ds.sizes["level"]}, axis=-1)
+expand_dict = {dim: ds.sizes[dim] for dim in ds.dims if dim != "level"}
+levs_expanded = ds["level"].expand_dims(dim=expand_dict)
+mask = levs_expanded <= sp_expanded
+
+### Step 3b: Get pressure for integration
+pa = 100 * ds.level.values # convert to Pa from hPa, used for integration
+
+
+### Step 3c: Calculate total energy components
+# sensible heat
+sensible_heat = cp * ds["T"]
+# latent heat - this is already column-integrated
+latent_heat = Lv * ds["TCW"]
+# geopotential energy
+geopotential_energy = g * ds["Z"]
+# kinetic energy
+kinetic_energy = 0.5 * ds["U"] ** 2 + 0.5 * ds["V"] ** 2
+
+### Step 3d: Calculate total energy by adding all components
+# total energy minus latent heat
+dry_total_energy = sensible_heat + geopotential_energy + kinetic_energy
+dry_total_energy = dry_total_energy.where(mask, np.nan)
+# column integration
+dry_total_energy_column = (1 / g) * scipy.integrate.trapezoid(
+    dry_total_energy, pa, axis=3
 )
 
-# VAR_OLR over range of times
-titles = [f"OLR at {np.datetime_as_string(time, unit='h')} UTC" for time in olr_ds["init_time"].values]
-vis.create_and_plot_variable_gif(
-    data=olr_ds["VAR_OLR"],
-    plot_var="VAR_OLR",
-    iter_var="init_time",
-    iter_vals=[0, 1],
-    plot_dir=plot_dir,
-    units="W/m^2",
-    cmap="magma",
-    titles=titles,
-    keep_images=True,
-    dpi=300,
-    fps=1,
+# sum
+ds["VAR_TE"] = (expand_dict, dry_total_energy_column + latent_heat.values)
+ds["VAR_TE"] = ds["VAR_TE"].assign_attrs(
+    {"units": "J/m^2", "long_name": "Total Energy"}
 )
-######################################
+
+### Step 3e: Weight by latitude
+# get latitude weighted total energy (time, ensemble)
+ds["LW_TE"] = inference.latitude_weighted_mean(ds["VAR_TE"], ds.latitude)
+ds["LW_TE"].assign_attrs(
+    {"units": "J/m^2", "long_name": "Latitude-Weighted Total Energy"}
+)
+
+### Step 3f: Calculate the time derivative of the total energy
+# use second order central difference to calculate the time derivative
+# this computes time derivative w/r/t hours, so divide by 3600 to get W/m^2
+ds["dLW_TE_dt"] = ds["LW_TE"].differentiate("lead_time") / 3600
+ds["dLW_TE_dt"].assign_attrs(
+    {"units": "W/m^2", "long_name": "Time Derivative of Latitude-Weighted Total Energy"}
+)
+
+breakpoint()
+
 
 # ### Plot the results ######################
 # plot_var = "MEAN_SP"
