f7077528ba699924d4eb4c67d1c86039b8807263
Joshua Elms <joshua.elms111@gmail.com>
2025-08-29 20:19:13 -0400

    just adding configs

--- initial_conditions/make_HM24_ICs/submit_CDS.sh
+++ initial_conditions/make_HM24_ICs/submit_CDS.sh
@@ -9,8 +9,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=12:00:00
-#SBATCH --mem=250GB
+#SBATCH --time=48:00:00
+#SBATCH --mem=50GB
 #SBATCH -A r00389
 
 source /N/slate/jmelms/projects/earth2studio-cu126/.venv/bin/activate
