4167a1a60e9e9de24d4dcbb3281808cfad073af2
Joshua Elms <joshua.elms111@gmail.com>
2025-09-11 15:16:17 -0400

    adding untested TC exper and prepping to gen JAS regperts

---
+++ experiments/F.tropical_cyclone_hm24/1.run_experiment.py
@@ -0,0 +1,102 @@
+from utils import general
+from torch.cuda import mem_get_info
+from earth2studio.io import XarrayBackend
+import xarray as xr
+import numpy as np
+from pathlib import Path
+import warnings
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+def run_experiment(model_name: str, config_path: str) -> str:
+    # read config file
+    config = general.read_config(config_path)
+
+    print(f"Running experiment for model: {model_name}")
+    print(
+        f"GPU memory: {mem_get_info()[0] / 1e9:.2f} GB available out of {mem_get_info()[1] / 1e9:.2f} GB"
+    )
+
+    # unpack config & set paths
+    IC_params = config["initial_condition_params"]
+    season = IC_params["season"]
+    IC_path = Path(IC_params["HM24_IC_dir"]) / f"{model_name}.nc"
+    pert_params = config["perturbation_params"]
+    perturbation_path = (
+        Path(pert_params["perturbation_dir"])
+        / f"{season}_20N_320E_z-regression_{model_name}.nc"
+    )
+    output_dir = Path(config["experiment_dir"]) / config["experiment_name"]
+    nc_output_file = output_dir / f"output_{model_name}.nc"
+    tendency_file = output_dir / "auxiliary" / f"tendency_{model_name}.nc"
+
+    # load the model
+    model = general.load_model(model_name)
+    
+    # some models (SFNO, FCN3, ...) need to be told to hold the solar zenith angle constant
+    model.const_sza = True
+
+    # interface between model and data
+    xr_io = XarrayBackend()
+
+    # load the time-mean initial condition from HM24
+    IC_ds = xr.open_dataset(IC_path)
+    IC_ds = general.sort_latitudes(IC_ds, model_name, input=True)
+    data_source = general.DataSet(IC_ds, model_name)
+
+    # read and preprocess initial perturbation
+    pert = xr.open_dataset(perturbation_path)
+    pert = general.sort_latitudes(pert, model_name, input=True)
+    amp_vec = pert_params["amp"]
+
+    # run experiment in loop, applying diff pert amplitude each iteration
+    run_kwargs = {
+        "time": np.atleast_1d(np.datetime64("2000-01-01")),
+        "nsteps": config["n_timesteps"],
+        "prognostic": model,
+        "data": data_source,
+        "io": xr_io,
+        "device": config["device"],
+    }
+    
+    ds_list = []
+    for i, amp in enumerate(amp_vec):
+        print(f"Running with perturbation amplitude {amp} ({i+1} of {len(amp_vec)})")
+        pert_scaled = pert.copy() * amp
+
+        ds = general.run_deterministic_w_perturbations(
+            run_kwargs,
+            config["tendency_reversion"],
+            model_name,
+            tendency_file,
+            initial_perturbation=pert_scaled,
+        )
+        
+        # add amplitude dimension
+        ds = ds.expand_dims({"amplitude": [amp]})
+
+        ds_list.append(ds)
+
+    # combine all amplitudes into one dataset
+    ds = xr.concat(ds_list, dim="amplitude")
+    
+    # sort output
+    ds = general.sort_latitudes(ds, model_name, input=False)
+    
+    # for clarity
+    ds = ds.rename({"time": "init_time"})
+    
+    # add model dimension to enable opening with open_mfdataset
+    ds = ds.assign_coords(model=model_name)
+
+    # save data
+    ds.to_netcdf(nc_output_file)
+
+
+if __name__ == "__main__":
+    general.run_experiment_controller(
+        calling_directory=Path(__file__).parent,
+        run_experiment=run_experiment,
+        config_path=Path(__file__).parent / "0.config.yaml"
+    )
--- experiments/F.tropical_cyclone_hm24/2.run_analysis.py
+++ experiments/F.tropical_cyclone_hm24/2.run_analysis.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-print("Visualizing results of HM24 ETC Experiment")
+print("Visualizing results of HM24 TC Experiment")
 
 config = general.read_config(Path("0.config.yaml"))
     
@@ -22,9 +22,12 @@ plot_dir = output_dir / "plots"
 
 # convenience vars
 n_timesteps = config["n_timesteps"]
+lead_times_h = np.arange(0, 6*n_timesteps+1, 6)
 g = 9.81 # m/s^2
 
 for model_name in models:
+    if model_name == "Pangu24":
+        exit()
     print(f"Visualizing {model_name}")
     IC_path = Path(IC_params["HM24_IC_dir"]) / f"{model_name}.nc"
     perturbation_path = Path(pert_params["perturbation_dir"]) / f"{season}_40N_150E_z-regression_{model_name}.nc"
@@ -35,12 +38,8 @@ for model_name in models:
     tds = xr.open_dataset(tendency_file)
     mean_ds = xr.open_dataset(IC_path)
     mean_ds = general.sort_latitudes(mean_ds, model_name, input=False)
-    # some models use only 720 lats instead of 721
     if mean_ds.sizes["lat"] > ds.sizes["lat"]:
         mean_ds = mean_ds.isel(lat=slice(1, ds.sizes["lat"] + 1))
-        
-    if mean_ds.sizes["time"] > 1:
-        mean_ds = mean_ds.isel(time=0) # only need one time slice for mean state
     print("Dividing geopotential Z [m^2/s^2] by 9.8 [m/s^2] to convert to height z [m]")
     for level in model_info.STANDARD_13_LEVELS:
         try:
@@ -69,80 +68,76 @@ for model_name in models:
     #
     ### Begin Extratropical Cyclone Visualizations ###
     #
-    if ds.sizes["lead_time"] >= 16:
-        ### begin HM24 Fig. 3
-        # 500Z plot
-        plot_vec = True
 
-        projection = ccrs.Robinson(central_longitude=-90.)
-        fig, ax = plt.subplots(nrows=4,ncols=1,figsize=(11*2,8.5*2),subplot_kw={'projection': projection}, layout='constrained')
+    ### begin HM24 Fig. 3
+    # 500Z plot
+    plot_vec = True
 
-        panel_label = ['(A)','(B)','(C)','(D)']
+    projection = ccrs.Robinson(central_longitude=-90.)
+    fig, ax = plt.subplots(nrows=4,ncols=1,figsize=(11*2,8.5*2),subplot_kw={'projection': projection}, layout='constrained')
 
-        axi = -1
-        
-        lat, lon = ds["lat"].values, ds["lon"].values
-        for it in [0,48,72,96]: # lead times in hours
-            axi+=1
+    panel_label = ['(A)','(B)','(C)','(D)']
 
-            # h&m24 plot replication
-            # _mean indicates the mean state
-            # _pert indicates the perturbed run
-            # _anom indicates the anomaly (perturbated run - mean state)
-
-            z500_mean = mean_ds["z500"].squeeze().values
-            u500_mean = mean_ds["u500"].squeeze().values
-            v500_mean = mean_ds["v500"].squeeze().values
-            z500_pert = ds["z500"].sel(lead_time=np.timedelta64(it, "h")).squeeze().values
-            u500_pert = ds["u500"].sel(lead_time=np.timedelta64(it, "h")).squeeze().values
-            v500_pert = ds["v500"].sel(lead_time=np.timedelta64(it, "h")).squeeze().values
-            pzdat = z500_pert - z500_mean
-            udat  = u500_pert - u500_mean
-            vdat  = v500_pert - v500_mean
-            basefield = z500_mean
-
-            dcint = 20; ncint=5
-            vscale = 250 # vector scaling (counterintuitive:smaller=larger arrows)
-
-            if plot_vec:
-                # Plot vectors on the map
-                latskip = 10
-                lonskip = 10
-                alpha = 1.0
-                col = 'g'
-                cs = ax[axi].quiver(lon[::lonskip],lat[::latskip],udat[::latskip,::lonskip],vdat[::latskip,::lonskip],transform=ccrs.PlateCarree(),scale=vscale,color=col,alpha=alpha)
-                qk = ax[axi].quiverkey(cs, 0.65, 0.01, 10., r'$10~ m/s$', labelpos='E',coordinates='figure',color=col)
-
-            # mean state or full field
-            alpha = 1.0
-            cints = np.arange(4800,6000,60.)
-            cs = ax[axi].contour(lon,lat,basefield,levels=cints,colors='0.5',transform=ccrs.PlateCarree(),alpha=alpha)
-            # perturbations
-            alpha = 1.0
-            cints = list(np.arange(-ncint*dcint,-dcint+.001,dcint))+list(np.arange(dcint,ncint*dcint+.001,dcint))
-            cints_neg = list(np.arange(-ncint*dcint,-dcint+.001,dcint))
-            cints_pos = list(np.arange(dcint,ncint*dcint+.001,dcint))
-            lw = 2.
-            cs = ax[axi].contour(lon,lat,pzdat,levels=cints_neg,colors='b',linestyles='solid',linewidths=lw,transform=ccrs.PlateCarree(),alpha=alpha)
-            cs = ax[axi].contour(lon,lat,pzdat,levels=cints_pos,colors='r',linestyles='solid',linewidths=lw,transform=ccrs.PlateCarree(),alpha=alpha)
-
-            # colorize land
-            ax[axi].add_feature(cfeature.LAND,edgecolor='0.5',linewidth=0.5,zorder=-1)
-
-            # gridlines
-            gl = ax[axi].gridlines(crs=ccrs.PlateCarree(),linewidth=1.0,color='gray', alpha=0.5,linestyle='--', draw_labels=True)
-
-            ax[axi].set_extent([140, 260, 20, 70],crs=ccrs.PlateCarree()) # Pacific
-
-            ax[axi].text(130,20,f"{panel_label[axi]}:{it*6}hrs",transform=ccrs.PlateCarree())
-
-        fig.tight_layout()
-        plt.savefig(plot_dir/f'IVP_500_{model_name}.pdf',dpi=300,bbox_inches='tight')
-        print(f"Saved IVP_500_{model_name}.pdf to {plot_dir}.")
-        ### end HM24 Fig. 3
+    axi = -1
     
-    if model_name == "Pangu24":
-        continue # diff timestep, skip rest of analysis for now
+    lat, lon = ds["lat"].values, ds["lon"].values
+    for it in [0,8,12,16]:
+        axi+=1
+
+        # h&m24 plot replication
+        # _mean indicates the mean state
+        # _pert indicates the perturbed run
+        # _anom indicates the anomaly (perturbated run - mean state)
+        z500_mean = mean_ds["z500"].squeeze().values
+        u500_mean = mean_ds["u500"].squeeze().values
+        v500_mean = mean_ds["v500"].squeeze().values
+        z500_pert = ds["z500"].isel(lead_time=it).squeeze().values
+        u500_pert = ds["u500"].isel(lead_time=it).squeeze().values
+        v500_pert = ds["v500"].isel(lead_time=it).squeeze().values
+        pzdat = z500_pert - z500_mean
+        udat  = u500_pert - u500_mean
+        vdat  = v500_pert - v500_mean
+        basefield = z500_mean
+
+        dcint = 20; ncint=5
+        vscale = 250 # vector scaling (counterintuitive:smaller=larger arrows)
+
+        if plot_vec:
+            # Plot vectors on the map
+            latskip = 10
+            lonskip = 10
+            alpha = 1.0
+            col = 'g'
+            cs = ax[axi].quiver(lon[::lonskip],lat[::latskip],udat[::latskip,::lonskip],vdat[::latskip,::lonskip],transform=ccrs.PlateCarree(),scale=vscale,color=col,alpha=alpha)
+            qk = ax[axi].quiverkey(cs, 0.65, 0.01, 10., r'$10~ m/s$', labelpos='E',coordinates='figure',color=col)
+
+        # mean state or full field
+        alpha = 1.0
+        cints = np.arange(4800,6000,60.)
+        cs = ax[axi].contour(lon,lat,basefield,levels=cints,colors='0.5',transform=ccrs.PlateCarree(),alpha=alpha)
+        # perturbations
+        alpha = 1.0
+        cints = list(np.arange(-ncint*dcint,-dcint+.001,dcint))+list(np.arange(dcint,ncint*dcint+.001,dcint))
+        cints_neg = list(np.arange(-ncint*dcint,-dcint+.001,dcint))
+        cints_pos = list(np.arange(dcint,ncint*dcint+.001,dcint))
+        lw = 2.
+        cs = ax[axi].contour(lon,lat,pzdat,levels=cints_neg,colors='b',linestyles='solid',linewidths=lw,transform=ccrs.PlateCarree(),alpha=alpha)
+        cs = ax[axi].contour(lon,lat,pzdat,levels=cints_pos,colors='r',linestyles='solid',linewidths=lw,transform=ccrs.PlateCarree(),alpha=alpha)
+
+        # colorize land
+        ax[axi].add_feature(cfeature.LAND,edgecolor='0.5',linewidth=0.5,zorder=-1)
+
+        # gridlines
+        gl = ax[axi].gridlines(crs=ccrs.PlateCarree(),linewidth=1.0,color='gray', alpha=0.5,linestyle='--', draw_labels=True)
+
+        ax[axi].set_extent([140, 260, 20, 70],crs=ccrs.PlateCarree()) # Pacific
+
+        ax[axi].text(130,20,f"{panel_label[axi]}:{it*6}hrs",transform=ccrs.PlateCarree())
+
+    fig.tight_layout()
+    plt.savefig(plot_dir/f'IVP_500_{model_name}.pdf',dpi=300,bbox_inches='tight')
+    print(f"Saved IVP_500_{model_name}.pdf to {plot_dir}.")
+    ### end HM24 Fig. 3
 
     ### make gifs
     # Z500 anomalies (global)
