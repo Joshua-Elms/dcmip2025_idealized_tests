2ee811462b2dd66896b7f358748764cc2eec4b20
Joshua Elms <joshua.elms111@gmail.com>
2025-10-23 15:38:59 -0400

    minor vis mods to limits

--- experiments/A.mass_conservation/2.run_analysis.py
+++ experiments/A.mass_conservation/2.run_analysis.py
@@ -22,7 +22,7 @@ plot_var = "msl" # choose either "msl" or "ssp", can only use "ssp" if models = 
 cmap_str = "Dark2" # options here: matplotlib.org/stable/tutorials/colors/colormaps.html, if "single:" is included, only one color will be used
 day_interval_x_ticks = 1 # how many days between x-ticks on the plot
 spec_int = 5e-4
-individual_standardized_ylims = (1000, 1120) # (1011.5 - 1.5, 1011.5 + 1.5) # y-limits for the plot, set to None to use the model output min/max, normally (1010, 1014)
+individual_standardized_ylims = (900, 1130) # (1011.5 - 1.5, 1011.5 + 1.5) # y-limits for the plot, set to None to use the model output min/max, normally (1010, 1014)
 mae_standardized_ylims = (0, 120) # y-limits for the MAE plot, set to None to use the model output min/max, normally (0, 5)
 mae_ground_truth = 1011.5 # set None to use IC as ground truth for comparison or a number to use as a constant ground truth value
 show_legend = True
@@ -101,8 +101,10 @@ for model in models:
         ytick_interval = 0.5
     elif val_diff <= 15.0:
         ytick_interval = 2.0
-    else:
+    elif val_diff <= 150.0:
         ytick_interval = 10.0
+    else:
+        ytick_interval = 20.0
     yticks = np.arange(val_range[0], val_range[1]+ytick_interval, ytick_interval)
     ax.set_yticks(yticks, yticks, fontsize=smallsize)
     ax.set_xlabel("Simulation Time (days)", fontsize=fontsize)
